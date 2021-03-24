4bbb9002f0c69b8d390cff1374e177ec40522f91
Abhishek <abhishekdixit44@gmail.com>
2021-03-25 00:31:13 +0530

    conflict commit

--- GitMyJava/src/CTL/MyTest.java
+++ GitMyJava/src/CTL/MyTest.java
@@ -5,7 +5,8 @@ public class MyTest {
 	public static void main(String[] args) {
 		
 		System.out.println("in git java");
-		System.out.println("in git java Abhi");
+		System.out.println("in git java Abhi Dixit");
+		System.out.println("Conflict");
 	}
 
 }
