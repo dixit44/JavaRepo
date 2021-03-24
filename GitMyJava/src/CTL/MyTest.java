7136cdfdfb3ed77561d506cb855439b154743c44
Abhishek <abhishekdixit44@gmail.com>
2021-03-25 00:35:57 +0530

    cccc

--- GitMyJava/src/CTL/MyTest.java
+++ GitMyJava/src/CTL/MyTest.java
@@ -5,8 +5,6 @@ public class MyTest {
 	public static void main(String[] args) {
 		
 		System.out.println("in git java");
-		System.out.println("in git java Abhi Dixit");
-
 		System.out.println("Conflict");
 
 
