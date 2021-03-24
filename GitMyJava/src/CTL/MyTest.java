7ec0245ed73e1cc25244dc2cbf7434464a61b7ce
dixit44 <81325254+dixit44@users.noreply.github.com>
2021-03-25 00:34:08 +0530

    Update MyTest.java

--- GitMyJava/src/CTL/MyTest.java
+++ GitMyJava/src/CTL/MyTest.java
@@ -5,9 +5,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		
 		System.out.println("in git java");
-		System.out.println("in git java Abhi Dixit");
-
-		System.out.println("Conflict");
+		
 
 
 	}
